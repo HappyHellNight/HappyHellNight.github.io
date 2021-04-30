483db7c192961279115beab52059b1f6866f6703
@kszotov <kszotov@gmail.com>
2021-04-30 09:18:04 +0300

    LB3 general

---
+++ i4.js
@@ -0,0 +1,6 @@
+var SecondName="Зотов";
+document.getElementById("sname").innerText=SecondName;
+function vozvrat()
+{
+document.getElementById("sname").innerText = document.getElementById("txt").value
+}
